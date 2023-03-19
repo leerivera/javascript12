0b0f18107bd9c5c908c94dff85e33608017ad1c6
leerivera <leerivera80@gmail.com>
2023-03-19 18:41:49 -0400

    love

--- class-weekend-boring/js/main.js
+++ class-weekend-boring/js/main.js
@@ -5,6 +5,9 @@ function check() {
   const day = document.querySelector('#day').value
 
   //Conditionals go here
+  if (day === 'Saturday' || day === 'Sunday') {
+    document.querySelector('#placeToPutResult').innerHTML = 'It\'s the weekend!'
+  }
 
 
 }
